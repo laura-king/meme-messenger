8f9b3c5a421aa122df22ab1a126211cbd5f881ed
Laura King <lking@mitre.org>
2017-09-22 15:15:03 -0400

    basic views

--- app.py
+++ app.py
@@ -9,5 +9,19 @@ def main_page():
 	"""
 	return render_template('base_page.html')
 
+@app.route('/welcome')
+def welcome_page():
+	"""
+	Loads welcome page
+	"""
+	return render_template('welcome.html')
+
+@app.route('/account')
+def account_page():
+	"""
+	Loads account page
+	"""
+	return render_template('account_page.html')
+
 if __name__ == "__main__":
 	app.run()
