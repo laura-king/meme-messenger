4e20880e862868d4dc4938ff3c33de3ef6c434b0
Bill Tarr <betarr13@gmail.com>
2017-09-25 19:01:06 -0400

    autopep8 app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
 
+
 @app.route('/')
 def main_page():
-	"""
-	Loads main page
-	"""
-	return render_template('base_page.html')
+    """
+    Loads main page
+    """
+    return render_template('base_page.html')
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
