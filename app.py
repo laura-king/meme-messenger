de3ddf75ed3ec12cdae9e81375afd365a003204a
Bill Tarr <betarr13@gmail.com>
2017-09-26 17:24:39 -0400

    autopep8 app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main_page():
     """
     Loads main page
     """
     return render_template('base_page.html')
+
 
 @app.route('/welcome')
 def welcome_page():
@@ -16,21 +18,26 @@ def welcome_page():
     """
     return render_template('welcome.html')
 
+
 @app.route('/account/<username>')
 def account_page(username):
     """
     Loads account page
     """
-    #See if this user is the user looking
-    #Check to see if a user exists with that name
-    existing_user=True
-    user_data = {"username":username}
+    # See if this user is the user looking
+    # Check to see if a user exists with that name
+    existing_user = True
+    user_data = {"username": username}
     if existing_user:
-        #Temp List for now
+        # Temp List for now
         blocked = ['BigJim', 'Jerkface420', 'GuyFerrari']
-        #Obtain the user's information somehow and package it in a dictonary
-        user_data.update({"blocked_users": blocked, "privacy":'friends'})
-    return render_template('account_page.html', user_data=user_data, existing_user=existing_user)
+        # Obtain the user's information somehow and package it in a dictonary
+        user_data.update({"blocked_users": blocked, "privacy": 'friends'})
+    return render_template(
+        'account_page.html',
+        user_data=user_data,
+        existing_user=existing_user)
+
 
 if __name__ == "__main__":
     app.run()
